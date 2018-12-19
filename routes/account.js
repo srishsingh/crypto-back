3123982a45192534dd4800fc29972939bc578e67
Shubham Mehrotra <shubhammehrotra.symfony@webkul.com>
2018-12-19 11:12:03 +0530

    updates

--- routes/account.js
+++ routes/account.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(requset, response, next) {
-    response.send("true");
+/* api for signup. */
+router.post('/signup', function(request, response, next) {
+    response.setHeader('Content-Type', 'application/json');
+    response.send(JSON.stringify({ action: 'signup' }));
+});
+
+/* api for signin. */
+router.post('/signin', function(request, response, next) {
+    response.setHeader('Content-Type', 'application/json');
+    response.send(JSON.stringify({ action: 'signin' }));
 });
 
 module.exports = router;
